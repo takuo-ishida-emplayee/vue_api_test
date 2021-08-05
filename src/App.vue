339d9c07e527303d9cf3f4fa77dfc9ecc59ca596
emplayee_ishida <ishida_takuo@emplayee.co.jp>
2021-08-05 22:34:59 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ status }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      status: 0,
+    };
+  },
+  async mounted() {
+    console.log(process.env.VUE_APP_API);
+    const data = await axios.get(process.env.VUE_APP_API);
+    console.log(data.status);
+    this.status = data.status;
+  },
+};
 </script>
 
 <style>
